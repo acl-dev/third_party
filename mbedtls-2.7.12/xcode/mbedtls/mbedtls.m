bb7e304a5be25072ca576d839d49d92496a20326
shuxin 　　zheng <zhengshuxin@gmail.com>
2022-05-09 09:48:57 +0800

    optimize mbedtls projects

---
+++ mbedtls-2.7.12/xcode/mbedtls/mbedtls.m
@@ -0,0 +1,13 @@
+//
+//  mbedtls.m
+//  mbedtls
+//
+//  Created by shuxin 　　zheng on 2019/9/29.
+//  Copyright © 2019 iqiyi. All rights reserved.
+//
+
+#import "mbedtls.h"
+
+@implementation mbedtls
+
+@end
